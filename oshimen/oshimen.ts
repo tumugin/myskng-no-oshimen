1ec8eac5fcdaf3216daede49042fb8c676e1fa55
Kazuki Oishi <oishikazuki@gmail.com>
2024-10-19 06:21:27 +0900

    add oshimen detail page

--- oshimen/oshimen.ts
+++ oshimen/oshimen.ts
@@ -1,11 +1,29 @@
 export interface Oshimen {
+  /**
+   * アイドルの一意なID
+   */
   id: string
+  /**
+   * アイドルの名前
+   */
   name: string
+  /**
+   * アイドルの誕生日。誕生日特殊処理に必要なので必ず正確な誕生日を入れること。
+   */
   birthday: {
     month: number
     day: number
   }
+  /**
+   * 短い愛文
+   */
   shortDescription: string
+  /**
+   * 詳細な愛文
+   */
   description: string
+  /**
+   * 表示させるツイートID
+   */
   tweetId: string
 }
