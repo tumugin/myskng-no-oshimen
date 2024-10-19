bd04fb988d11a6a5aedee38d0b6c71495e516e19
Kazuki Oishi <oishikazuki@gmail.com>
2024-10-20 07:27:14 +0900

    推しメンを追加

--- oshimen/oshimens.ts
+++ oshimen/oshimens.ts
@@ -27,16 +27,38 @@ export const oshimens: Oshimen[] = [
     emoji: '💎',
     birthday: { month: 1, day: 30 },
     shortDescription: 'ｵﾆﾙｻﾝ...',
-    description: 'やはりｵﾆﾙｻﾝ...なんだよなぁ... ',
+    description:
+      'やはりｵﾆﾙｻﾝ...なんだよなぁ...。限界オタクなところも良い。いつも楽しい。いつもかわいい。',
     tweetId: '1814957022355579164',
+  },
+  {
+    id: 'chia',
+    name: '未白ちあ',
+    emoji: '🐹',
+    birthday: { month: 2, day: 23 },
+    shortDescription: 'ﾁｱﾁｬﾝ... かわいいね。',
+    description:
+      'やはりﾁｱﾁｬﾝ... かわいいね。ちあにあちあちなので、ぼくは。最近、ちあが可愛すぎて心が苦しいです。ガチで好きなんだなぁ...とね。',
+    tweetId: '1837825076810731542',
   },
   {
     id: 'mayuri',
     name: '仔羽まゆり',
     emoji: '🤍',
-    birthday: { month: 5, day: 5 },
-    shortDescription: 'ﾏﾕﾘﾁｬﾝ...',
-    description: 'やはりﾏﾕﾘﾁｬﾝ...',
+    birthday: { month: 6, day: 26 },
+    shortDescription: 'ﾏﾕﾘﾁｬﾝ... ｶﾜｲｲﾈ...',
+    description:
+      'やはりﾏﾕﾘﾁｬﾝ...妹のようにｶﾜｲｲﾈ...ﾏﾕﾘﾁｬﾝ...。いつも笑顔で、いつもかわいい。守りたいこの笑顔。',
     tweetId: '1846154507270635774',
+  },
+  {
+    id: 'lilxusaki',
+    name: '李縷さき',
+    emoji: '🦭',
+    birthday: { month: 8, day: 2 },
+    shortDescription: 'ｻｷﾁｬﾝ... ｶﾜｲｲﾈ...',
+    description:
+      '最近、りるさきが気になってるんですよね。もやしくん呼びが可愛すぎる。りるさきはいいぞ。',
+    tweetId: '1847257456944292140',
   },
 ]
