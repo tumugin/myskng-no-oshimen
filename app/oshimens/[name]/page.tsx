26fee50d7819c5588f71a8b3732b087001000258
Kazuki Oishi <oishikazuki@gmail.com>
2024-10-20 11:38:09 +0900

    fixed ogps

--- app/oshimens/[name]/page.tsx
+++ app/oshimens/[name]/page.tsx
@@ -1,6 +1,7 @@
 import { oshimens } from '@/oshimen/oshimens'
 import { notFound } from 'next/navigation'
 import { OshimenPage } from '@/components/Page/OshimenPage'
+import type { Metadata } from 'next'
 
 export default function Page({ params }: { params: { name: string } }) {
   const oshimen = oshimens.find((v) => v.id === params.name)
@@ -15,4 +16,25 @@ export default function Page({ params }: { params: { name: string } }) {
 // SSG
 export const generateStaticParams = async () => {
   return oshimens.map((v) => ({ params: { name: v.id } }))
+}
+
+export function generateMetadata({
+  params,
+}: {
+  params: { name: string }
+}): Metadata {
+  const oshimen = oshimens.find((v) => v.id === params.name)
+
+  if (!oshimen) {
+    notFound()
+  }
+
+  return {
+    title: `もやしきんぐの推しメンは？ - ${oshimen.name}`,
+    description: oshimen.description,
+    openGraph: {
+      title: `もやしきんぐの推しメンは？ - ${oshimen.name}`,
+      description: oshimen.shortDescription,
+    },
+  }
 }
--- app/oshimens/page.tsx
+++ app/oshimens/page.tsx
@@ -1,6 +1,16 @@
 import { OshimenListPage } from '@/components/Page/OshimenListPage'
 import { oshimens } from '@/oshimen/oshimens'
+import type { Metadata } from 'next'
 
 export default function Page() {
   return <OshimenListPage oshimens={oshimens} />
+}
+
+export const metadata: Metadata = {
+  title: 'もやしきんぐの推しメンは？ - 推しメン一覧',
+  description: 'もやしきんぐの推しメンを解説するサイトです',
+  openGraph: {
+    title: 'もやしきんぐの推しメンは？ - 推しメン一覧',
+    description: 'もやしきんぐの推しメンを解説するサイトです',
+  },
 }
