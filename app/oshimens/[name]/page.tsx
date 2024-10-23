76678720a2f48b898fade91da5414df331e47597
Kazuki Oishi <oishikazuki@gmail.com>
2024-10-24 06:08:52 +0900

    Upgrade to Next.js 15

--- app/oshimens/[name]/page.tsx
+++ app/oshimens/[name]/page.tsx
@@ -3,7 +3,10 @@ import { notFound } from 'next/navigation'
 import { OshimenPage } from '@/components/Page/OshimenPage'
 import type { Metadata } from 'next'
 
-export default function Page({ params }: { params: { name: string } }) {
+export default async function Page(props: {
+  params: Promise<{ name: string }>
+}) {
+  const params = await props.params
   const oshimen = oshimens.find((v) => v.id === params.name)
 
   if (!oshimen) {
@@ -18,11 +21,10 @@ export const generateStaticParams = async () => {
   return oshimens.map((v) => ({ params: { name: v.id } }))
 }
 
-export function generateMetadata({
-  params,
-}: {
-  params: { name: string }
-}): Metadata {
+export async function generateMetadata(props: {
+  params: Promise<{ name: string }>
+}): Promise<Metadata> {
+  const params = await props.params
   const oshimen = oshimens.find((v) => v.id === params.name)
 
   if (!oshimen) {
